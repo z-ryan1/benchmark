1842165c2217fd838b544cfb7a3d39f973d54478
Gerald Shen <gerald.shen@mail.utoronto.ca>
2021-12-20 11:52:05 -0500

    min rounds should be 500

--- run_bench.sh
+++ run_bench.sh
@@ -13,5 +13,5 @@ do
     fi
     name="$(hostname -s)_${gpu_name}_$i" 
 
-    pytest test_bench.py --benchmark-save-data -k "hf_Longformer-cuda-eager or BERT_pytorch-cuda-eager or nvidia_deeprecommender-cuda-eager or resnet50-cuda-eager or dlrm-cuda-eager or timm_efficientnet-cuda-eager or drq-cuda-eager or soft_actor_critic-cuda-eager" --ignore_machine_config --benchmark-autosave --benchmark-warmup=on --cache-clear --benchmark-min-rounds=300 --benchmark-json="$name".json | tee "$name".out
+    pytest test_bench.py --benchmark-save-data -k "hf_Longformer-cuda-eager or BERT_pytorch-cuda-eager or nvidia_deeprecommender-cuda-eager or resnet50-cuda-eager or dlrm-cuda-eager or timm_efficientnet-cuda-eager or drq-cuda-eager or soft_actor_critic-cuda-eager" --ignore_machine_config --benchmark-autosave --benchmark-warmup=on --cache-clear --benchmark-min-rounds=500 --benchmark-json="$name".json | tee "$name".out
 done
