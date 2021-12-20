988d138ad8eae74b0cca264a35458f00ecf10307
Gerald Shen <gerald.shen@mail.utoronto.ca>
2021-12-20 11:21:34 -0500

    add ben run script

---
+++ run_bench.sh
@@ -0,0 +1,17 @@
+set -e
+gpu_name=$(nvidia-smi --format=csv,noheader --query-gpu=name --id=0 | sed 's/ /_/g')
+
+for i in TF32 FP32
+do
+    if test "$i" = "TF32"
+    then
+	unset NVIDIA_TF32_OVERRIDE
+	echo running benchmark with "$i"
+    else
+	export NVIDIA_TF32_OVERRIDE=0
+	echo running benchmark with "$i"
+    fi
+    name="$(hostname -s)_${gpu_name}_$i" 
+
+    pytest test_bench.py --benchmark-save-data -k "hf_Longformer-cuda-eager or BERT_pytorch-cuda-eager or nvidia_deeprecommender-cuda-eager or resnet50-cuda-eager or dlrm-cuda-eager or timm_efficientnet-cuda-eager or drq-cuda-eager or soft_actor_critic-cuda-eager" --ignore_machine_config --benchmark-autosave --benchmark-warmup=on --cache-clear --benchmark-min-rounds=300 --benchmark-json="$name".json | tee "$name".out
+done
