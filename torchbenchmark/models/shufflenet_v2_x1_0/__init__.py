a843d87b820b6dd79163bb4fcbc36d721ca6225e
Gerald Shen <gerald.shen@mail.utoronto.ca>
2022-01-13 17:08:00 -0500

    remove unnecessary models

--- torchbenchmark/models/shufflenet_v2_x1_0/__init__.py
+++
@@ -1,9 +0,0 @@
-from torchbenchmark.util.framework.vision.model_factory import TorchVisionModel
-from torchbenchmark.tasks import COMPUTER_VISION
-
-class Model(TorchVisionModel):
-    task = COMPUTER_VISION.CLASSIFICATION
-
-    def __init__(self, device=None, jit=False, train_bs=128, eval_bs=64, extra_args=[]):
-        super().__init__(model_name="shufflenet_v2_x1_0", device=device, jit=jit,
-                         train_bs=train_bs, eval_bs=eval_bs, extra_args=extra_args)
